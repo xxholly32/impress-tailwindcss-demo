73b60fe44ce824a93a81d243dc16121bde33bfeb
xxholly32 <xxholly32@163.com>
2020-11-26 16:44:04 +0800

    fix: 🐛 fix build path issue

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/impress-demo/'
+    ? '/impress-tailwindcss-demo/'
     : '/',
   configureWebpack: {
     externals: {
